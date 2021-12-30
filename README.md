a2352c4eb27f8ff45ea99f69580b432ffa8610f4
Eliot New <91668519+eliotnew@users.noreply.github.com>
2021-12-30 17:37:03 +0000

    Entire files
    I have added EVERYTHING so far as of new years eve

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# StudyLamp1004
-This is my SPA
+# comp1004---main-assignment-eliotnew
+comp1004---main-assignment-eliotnew created by GitHub Classroom
+#I had to add some code that displays that the page is 'under construction'#
+I struggled with command line so caved and worked out how to push from visual studio. 
---
+++ wwwroot/js/spa.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $('#about').click(function () {
+        $('#page-content-wrapper').load('About.cshtml');
+    })
+});$(document).ready(function () {
+    $('#start').click(function () {
+        $('#page-content-wrapper').load('startstudy.cshtml');
+    })
+});$(document).ready(function () {
+    $('#studystats').click(function () {
+        $('#page-content-wrapper').load('studystats.cshtml');
+    })
+});
